78d4736f5d3bd5a53b1417d9977fb6295b43e0a5
Mez Charney <mez.charney@gmail.com>
2019-09-30 21:40:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -232,8 +232,8 @@ end
 def player_with_longest_name
   longest = ''
   longest_length = 0
-  game_hash.each do |location, team|
-    team[:players].each do |player|
+  game_hash.each do |home_away, keys|
+    keys[:players].each do |player|
       name_length = player[:player_name].length
       longest, longest_length = player[:player_name], name_length if name_length > longest_length
     end
