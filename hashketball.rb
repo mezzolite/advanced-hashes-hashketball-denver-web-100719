72dfca68399aa3f2e85e38f0c2b9fc9bd78a1dd0
Mez Charney <mez.charney@gmail.com>
2019-09-30 21:56:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -241,7 +241,7 @@ def player_with_longest_name
   return longest
 end
 
-def long_name_steals_a_ton
+def long_name_steals_a_ton?
   most_steals = ""
   steals = 0 
    game_hash.each do |location, team|
